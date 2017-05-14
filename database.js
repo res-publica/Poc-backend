b9f5dc3012d578e74538a377000800732231257d
Adrien Cantérot <adrien.canterot@gmail.com>
2017-05-14 19:27:01 +0200

    config.json for sequelizedb

--- database.js
+++ database.js
@@ -1,16 +1,26 @@
 //Sequelize setup code, works is pgsql
 
 var Sequelize = require('sequelize');
-let sequelize = new Sequelize('quotedb', 'adrien', '', {
-	host: 'localhost',
-	port: 5432,
-	dialect: 'postgres',
-	pool: {
-		max: 5,
-		min: 0,
-		idle: 10000
-	}
-});
+var env       = process.env.NODE_ENV || 'development';
+var config    = require(__dirname + '/config/config.json')[env];
+var db        = {};
+
+if (config.use_env_variable) {
+  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+} else {
+  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+}
+
+// let sequelize = new Sequelize('quotedb', 'adrien', '', {
+// 	host: 'localhost',
+// 	port: 5432,
+// 	dialect: 'postgres',
+// 	pool: {
+// 		max: 5,
+// 		min: 0,
+// 		idle: 10000
+// 	}
+// });
 
 //model definition, docs : http://docs.sequelizejs.com/manual/tutorial/models-definition.html
 var Quote = sequelize.define('quotes', {
