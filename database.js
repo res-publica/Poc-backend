84d6e225fd96f2bfeedf908173cde953f03dee47
Adrien Cantérot <adrien.canterot@gmail.com>
2017-05-14 19:48:43 +0200

    proper env

--- database.js
+++ database.js
@@ -5,10 +5,9 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/config/config.json')[env];
 var db        = {};
 
-console.log("URI : " + process.env[config.use_env_variable]);
-if (config.use_env_variable) {
-	console.log("URI : " + process.env[config.use_env_variable]);
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+if (process.env.NODE_ENV == 'production') {
+	
+  var sequelize = new Sequelize(config.uri);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
